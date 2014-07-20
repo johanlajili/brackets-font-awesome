1dfbc68a6c094a03c3b1d2f87d82bc2b5dd94ccd
Johan LAJILI <johan.lajili@gmail.com>
2014-07-20 15:50:29 +0200

    Interface work

--- js/bracketsFontAwesome.js
+++ js/bracketsFontAwesome.js
@@ -31,7 +31,7 @@ define(function (require, exports, module) {
 		window.setTimeout(this.sizeEditorToContent.bind(this));
 	};
 	BracketsFontAwesome.prototype.sizeEditorToContent = function () {
-		this.hostEditor.setInlineWidgetHeight(this, 220, true);
+		this.hostEditor.setInlineWidgetHeight(this, 280, true);
 	};
 
 
